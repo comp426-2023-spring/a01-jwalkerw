7d013cabfdc04b4505fdea49b1b1ebf0aad8b167
Jordan Wahab <jordan_wahab@unc.edu>
2023-04-11 18:56:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ var parseArgs = require("minimist");
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 var argv = parseArgs(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
-const port = argv.port;
 // Make this const default to port 3000 if there is no argument given for `--port`.
-port = port || 3000;
+const port = argv.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
